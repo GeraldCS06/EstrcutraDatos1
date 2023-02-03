3f4856b487d811099ba54a0711778a508e03be85
Gerald0609 <66327871+Gerald0609@users.noreply.github.com>
2023-02-03 11:26:22 -0600

    Recursividad

---
+++ src/main/java/com/mycompany/main/Recursividad0.java
@@ -0,0 +1,12 @@
+package com.mycompany.main;
+
+/**
+ *
+ * @author geral
+ */
+public class Recursividad0 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
